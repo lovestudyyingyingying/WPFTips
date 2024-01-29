c153b8b553bb662e58727e8ddba4b8b2d09d8c04
姚国强 <2396034319@qq.com>
2024-01-29 17:09:21 +0800

    datagrid实现数据筛选

---
+++ 实现数据筛选功能/SelectDataGridView/SelectDataGridView/Employee.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectDataGridView
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public static Employee FakeOne() => employeeFaker.Generate();
+        public static IEnumerable<Employee> FakeMany(int count)=>employeeFaker.Generate(count);
+
+        private static readonly Faker<Employee> employeeFaker = new Faker<Employee>().RuleFor(x => x.FirstName, x => x.Person.FirstName)
+            .RuleFor(x => x.LastName, x => x.Person.LastName).RuleFor(x => x.Age, x => x.Random.Int(20, 40));
+    }
+}
---
+++ 实现数据筛选功能/SelectDataGridView/SelectDataGridView/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SelectDataGridView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private List<Employee> employees;
+        //private ICollectionView collectionView;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //employees = Employee.FakeMany(10).ToList(); 
+            //collectionView = CollectionViewSource.GetDefaultView(employees);
+            //DataGrid1.ItemsSource = collectionView;
+            //collectionView.Filter = (item) =>
+            //{
+            //    var employee = item as Employee;
+            //    return employee.FirstName.Contains(tbxFliter.Text)|| employee.LastName.Contains(tbxFliter.Text);
+            //};
+            this.DataContext = new ViewModel(Employee.FakeMany(10));
+        }
+
+
+        //private void tbxFliter_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    collectionView.Refresh();
+        //}
+
+        //private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        //{
+        //    employees.Add(Employee.FakeOne());
+        //    collectionView.Refresh();
+        //}
+    }
+    public partial class ViewModel : ObservableObject
+    {
+        List<Employee> employees;
+
+        [ObservableProperty]
+        ICollectionView collectionView;
+
+        [ObservableProperty]
+        string fliterString;
+
+        partial void OnFliterStringChanged(string value)=> CollectionView.Refresh();
+        
+           
+        
+        public ViewModel(IEnumerable<Employee> employees)
+        {
+            this.employees = new List<Employee>(employees);
+            CollectionView = CollectionViewSource.GetDefaultView(employees);
+            CollectionView.Filter = (item) =>
+            {
+                if (string.IsNullOrEmpty(FliterString))
+                    return true;
+                var employee = item as Employee;
+                return employee.FirstName.Contains(FliterString) || employee.LastName.Contains(FliterString);
+            };
+        }
+
+
+    }
+}
